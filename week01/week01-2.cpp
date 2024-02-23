ae1e43b2c1990818433da3ff6ac1c4c233262a37
fancyboi0304 <rayfan0304@gmail.com>
2024-02-23 11:50:40 +0800

    week01

---
+++ week01/week01-2.cpp
@@ -0,0 +1,9 @@
+//week01-2.cpp F001 C/C++
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a+b); 
+
+}
---
+++ week01/week01-3.cpp
@@ -0,0 +1,8 @@
+///week01-3.cpp結合Hello World和%d
+///在CodeBlock裡,File-New-Empty空白檔
+///再Ctrl-s存檔成week01-3.cpp,寫程式
+#include <stdio.h>
+int main()
+{
+    printf("Hello World %d\n",100);
+}
