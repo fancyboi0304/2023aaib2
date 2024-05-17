29a4c264d129bf4ef0142434da54ec12e51db8a6
fancyboi0304 <rayfan0304@gmail.com>
2024-05-17 11:49:10 +0800

    week13

---
+++ week13/week13-1.cpp
@@ -0,0 +1,19 @@
+///LeetCode 1325 Delete Leaves With a Given Values
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* removeLeafNodes(struct TreeNode* root, int target) {
+    if(root==NULL) return NULL;
+    struct TreeNode* left = removeLeafNodes(root->left,target);
+    struct TreeNode* right = removeLeafNodes(root->right,target);
+    if(left==NULL && right==NULL && root->val==target) return NULL;
+
+    root->left = left;
+    root->right = right;
+    return root;
+}
